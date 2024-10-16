1d8ee1bbdcfde63e1822887c217be70cff10b61a
ealinaa <alinaadhikari108@gmail.com>
2024-10-16 16:38:17 +0545

    worked with components

--- components/SidebarItem.tsx
+++ components/SidebarItem.tsx
@@ -1,10 +1,23 @@
+import Link from 'next/link';
 import React from 'react'
+import { IconType } from 'react-icons'
+import { twMerge } from 'tailwind-merge';
+interface SidebarItemProps {
+    icon: IconType;
+    label:string;
+    active?: boolean;
+    href: string;
+}
 
 
-
-const SidebarItem = () => {
+const SidebarItem: React.FC <SidebarItemProps> = ({
+    icon: Icon,
+    label,
+    active,
+    href
+}) => {
   return (
-    <div>SidebarItem</div>
+    <Link href={href} className={twMerge(`flex flex-row h-auto items-center w-full gap-x-4 text-md font-medium cursor-pointer hover:text-white transition text-neutral-400 py-1`, active && 'text-white')}>SidebarItem</Link>
   )
 }
 
