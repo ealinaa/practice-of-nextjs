dd1713d3151da7dc865f0aba97616112f7f14225
ealinaa <alinaadhikari108@gmail.com>
2024-10-17 21:55:51 +0545

    worked with components

--- components/Library.tsx
+++ components/Library.tsx
@@ -25,7 +25,7 @@ size={20}
 className='text-neutral-400 cursor-pointer hover:text-white transition'/>
         </div>
         <div className='flex flex-col gap-y-2 mt-4 px-3'>
-            List of songs
+            List of songs!
 
         </div>
     </div>
