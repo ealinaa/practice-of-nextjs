fa450b32a186d01dc2fc6f553ddf1c01dee9bffd
ealinaa <alinaadhikari108@gmail.com>
2024-10-24 22:48:02 +0545

    worked with player functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import ModalProvider from "@/providers/ModalProvider";
 import ToasterProvider from "@/providers/ToasterProvider";
 import UserProvider from "@/providers/userProvider";
 import getSongsByUserId from "@/actions/getSongsByUserId";
+import Player from "@/components/Player";
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -35,6 +36,7 @@ export default async function RootLayout({
           <Sidebar songs = {userSongs}>
             {children}
           </Sidebar>
+          <Player/>
           </UserProvider>
         </SupabaseProvider>
       </body>
